42f06500fd1d91223577dd0eccdb14a3e50883a9
bennymathew <benny.mathew@student.moringaschool.com>
2023-10-07 18:01:16 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(duty = 'go to the office'){
+    return `This Monday, I will ${duty}.`;
+}
+
+function wrapAdjective(symbol = '*'){
+    return function(adjective){
+        return `You are ${symbol}${adjective}${symbol}!`
+    }
+
+}
